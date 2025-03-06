88904f8855d1d21157eae2413bdc43f0fb910f99
1U23IT036-harikrishnan <142397392+1U23IT036-harikrishnan@users.noreply.github.com>
2025-03-06 09:27:47 +0000

    done8

--- LinkedList.java
+++ LinkedList.java
@@ -4,93 +4,80 @@ public class LinkedList {
     class Node {
         int data;
         Node next;
-
+        
         public Node(int data) {
-this.data = data;
-this.next = null;
+            this.data = data;
+            this.next = null;
         }
     }
 
     Node head = null;
     Node tail = null;
 
+    // Method to add a node at the end of the linked list
     public void addNode(int data) {
         Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
             tail = newNode;
         } else {
-           tail.next = newNode;
+            tail.next = newNode;
             tail = newNode;
         }
     }
 
-public static void main(String[] args) {
+    // Method to insert a node at the end of the linked list
+    public void insertEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Method to display the linked list
+    public void displayList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
         LinkedList list = new LinkedList();
         Scanner sc = new Scanner(System.in);
 
-int pos=0;
-int data,data1;
         System.out.println("ENTER INTEGERS TO CREATE NODES (ENTER A NON-INTEGER TO EXIT):");
-
-        while (sc.hasNext())
-        {
-            if(sc.hasNextInt())
-            {
-              data = sc.nextInt();
-             list.addNode(data);
+        while (sc.hasNext()) {
+            if (sc.hasNextInt()) {
+                int data = sc.nextInt();
+                list.addNode(data);
+            } else {
+                sc.next(); // Consume the non-integer token
+                System.out.println("LINKED LIST CREATED");
+                System.out.println("LINKED LIST BEFORE INSERTING");
+                list.displayList();
+                System.out.println("ENTER THE ELEMENT TO BE INSERTED:");
+                if (sc.hasNextInt()) {
+                    int data1 = sc.nextInt();
+                    list.insertEnd(data1);
+                } else {
+                    System.out.println("Enter integers only");
+                    System.exit(0);
+                }
+                System.out.println("LINKED LIST AFTER INSERTING");
+                list.displayList();
             }
-          else
-          {
-            String temp=sc.next();
-            System.out.println("LINKED LIST CREATED");
-            System.out.println("LINKED LIST BEFORE INSERTING");
-            list.displayList();
-            System.out.println("ENTER THE ELEMENT TO BE INSERTED:");
-            if(sc.hasNextInt())
-            {
-            data1=sc.nextInt();
-            list.insertEnd(data1);
-            }
-            else{
-              System.out.println("Enter integers only");
-              System.exit(0);
-            }
-            System.out.println("LINKED LIST AFTER INSERTING");
-            list.displayList();
-            System.exit(0);
-          }
         }
-    }
-public void insertEnd(int data) 
-    {    
-        //Create a new node    
-        Node newNode = new Node(data);    
-        //Checks if the list is empty    
-        if(head == null) 
-        {    
-            //If list is empty, both head and tail will point to new node    
-            head = newNode;    
-            tail = newNode;    
-        }    
-        else 
-        {    
-            Node temp=head;
-            while( temp.next!= null) 
-            {    
-                temp = temp.next;    
-            }    
-temp.next=newNode;
-System.out.println("Element Inserted");
-        }    
-    }  
-
-public void displayList() {
-        Node current = head;
-        while (current != null) {
-System.out.print(current.data + " -> ");
-            current = current.next;
-        }
-System.out.println("null");
+        sc.close();
     }
 }
